89ae23b7d3c48ecf5cd9d7a010207299eb9a8121
Bang <nnb24.112003@gmail.com>
2026-03-09 01:28:05 +0700

    fix api url production

--- Frontend/js/config.js
+++ Frontend/js/config.js
@@ -1,3 +1,6 @@
+// export const ENV = {
+//     API_URL : `http://localhost:3000` 
+// }
 export const ENV = {
-    API_URL : `http://localhost:3000` 
+    API_URL: "https://api-e-commerce-js-node-nnb.onrender.com"
 }
